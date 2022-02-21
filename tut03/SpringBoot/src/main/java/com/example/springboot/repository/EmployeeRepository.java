dc9ef17e598c4e426fe155d96a67b32ef7533d99
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-02-21 09:20:55 +0700

    se2

--- tut03/SpringBoot/src/main/java/com/example/springboot/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springboot.repository;
-
-import com.example.springboot.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
