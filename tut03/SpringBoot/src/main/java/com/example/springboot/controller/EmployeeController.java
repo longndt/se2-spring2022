06d6b35839916549c18f17e7d9bb88b32acb7714
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-02-21 08:57:35 +0700

    se2

---
+++ tut03/SpringBoot/src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.model.Employee;
+import com.example.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class EmployeeController {
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @RequestMapping(value = "/")
+    public String getAllEmployee(Model model) {
+        List<Employee> employees = employeeRepository.findAll();
+        model.addAttribute("employees", employees);
+        return "employeeList";
+    }
+
+    @RequestMapping(value = "/{id}")
+    public String getEmployeeById(
+            @PathVariable(value = "id") Long id, Model model) {
+        Employee employee = employeeRepository.getById(id);
+        model.addAttribute("employee", employee);
+        return "employeeDetail";
+    }
+}
+
